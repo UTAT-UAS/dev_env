16dda77406354ed3f1b3b4339527143bdd51889c
Benjamin Low <benlow999@gmail.com>
2025-09-16 02:06:53 +0000

    fix: SDL_GAMECONROLLERCONFIG for Radiomaster Pocket

--- .devcontainer/profiles/ben.sh
+++ .devcontainer/profiles/ben.sh
@@ -2,7 +2,7 @@
 
 # Custom SDL config
 echo '#!/usr/bin/env bash' | sudo tee /usr/bin/qgc > /dev/null
-echo 'SDL_GAMECONTROLLERCONFIG="0300000009120000544f000011010000,OpenTX Radiomaster Pocket Joystick,a:b0,b:b1,x:b3,y:b4,back:b10,guide:b12,start:b11,leftstick:b13,rightstick:b14,leftshoulder:b6,rightshoulder:b7,leftx:a0,lefty:a1,rightx:a3,righty:a2,lefttrigger:a4,righttrigger:a5,platform:Linux" /QGroundControl-x86_64.AppImage --appimage-extract-and-run' | sudo tee -a /usr/bin/qgc > /dev/null
+echo 'SDL_GAMECONTROLLERCONFIG="0300000009120000544f000011010000,OpenTX Radiomaster Pocket Joystick,a:b0,b:b1,x:a5,y:a6,back:b10,guide:b12,start:b11,leftstick:b13,rightstick:b14,leftshoulder:a4,rightshoulder:a7,leftx:a3,lefty:a2~,rightx:a0,righty:a1~,lefttrigger:a4,righttrigger:a5,platform:Linux" /QGroundControl-x86_64.AppImage --appimage-extract-and-run' | sudo tee -a /usr/bin/qgc > /dev/null
 
 # Personal aliases
 echo "set -o vi
